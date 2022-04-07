31706d2460cb44eec31e8390b1543df569c7396c
ulises avila camargo <ulisesavila04@gmail.com>
2022-04-07 15:49:38 -0500

    can get now all the emails from an account and prepare sending the newsletter to them

--- lib/api/email.ex
+++ lib/api/email.ex
@@ -4,4 +4,21 @@ defmodule Api.Email do
   def get_all_lists() do
     Mailchimp.Account.get() |> elem(1) |> Mailchimp.Account.lists()
   end
+
+  def get_all_contacts_from_a_list(list) do
+    Mailchimp.List.members(list, %{})
+  end
+
+  def send_emails(emails, content) do
+    IO.inspect("I have sent all emails")
+  end
+
+  def send_newsletter_to_cyphraium(content) do
+    get_all_lists()
+    |> elem(1)
+    |> Enum.find(fn element -> element.name === "Cyphraium" end)
+    |> get_all_contacts_from_a_list()
+    |> elem(1)
+    |> send_emails("a testo")
+  end
 end
