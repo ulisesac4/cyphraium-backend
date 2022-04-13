f3e0f69fd3ff21b745997480f86de51a748dc7e3
ulises avila camargo <ulisesavila04@gmail.com>
2022-04-13 16:49:11 -0500

    add service file and deployment of the api

---
+++ deploy_prod.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Initial setup
+mix deps.get --only prod
+MIX_ENV=prod mix compile
+
+# Build the release and overwrite the existing release directory
+MIX_ENV=prod mix release --overwrite
+
+# Remove old files
+
+sudo rm -rf /var/apps/cyphraium/*
+
+# Copy to deploy to place
+sudo cp -rf _build/prod/* /var/apps/cyphraium
+
+
+
+# Perform any migrations necessary
+_build/prod/rel/backend/bin/backend eval "Backend.Release.migrate"
+
+# Copy env vars to palce
+#sudo cp meddicit.env /etc/default/
+
+# Copy service to place
+sudo cp meddicit.service /etc/systemd/system/
+sudo chown -R meddicit:meddicit /var/apps/meddicit
+
+# Let binary to bind port 80
+
+sudo setcap "cap_net_bind_service=+ep" /var/apps/meddicit/rel/backend/erts-11.0.3/bin/beam.smp
