0761f03d650865ea9d21dda866a128c4f8d31e84
ulises avila camargo <ulisesavila04@gmail.com>
2022-04-14 12:30:33 -0500

    fix a naming

--- renew_certs.sh
+++ renew_certs.sh
@@ -1,15 +1,15 @@
 # generate certs
-sudo certbot certonly --standalone -d cyphraium.com  --agree-tos
+sudo certbot certonly -d cyphraium.com 
 
 # stop app
 sudo systemctl stop cyphraium.service 
 
 #remove old certs
 
-sudo rm -rf /home/cyphraium/Application/Cyphraium/certs/*
+sudo rm -rf /home/cyphraium/Application/Cyphraium/certs/.
 
 # copy certs to neccesary folder
-sudo cp /etc/letsencrypt/live/cyphraium.com/* /home/cyphraium/Application/Cyphraium/certs
+sudo cp /etc/letsencrypt/live/cyphraium.com/. /home/cyphraium/Application/Cyphraium/certs
 
 # do not forget to change permissions
 sudo chown -R cyphraium:cyphraium /home/cyphraium/Application/Cyphraium/certs
